66793f9568b6d5596002941acaf408faf7c52893
Santosk Kumar <santosh.java90@gmail.com>
2016-10-25 22:28:31 +0530

    Persistent Layer Initial Setup for vegie

---
+++ src/main/java/com/vgs/pl/dao/UserDao.java
@@ -0,0 +1,10 @@
+package com.vgs.pl.dao;
+
+/**
+ * Created by SantoshKumar.Maddela on 10/25/2016.
+ */
+public interface UserDao {
+
+    public Object findUser(Object userInfo) throws Exception;
+
+}
---
+++ src/main/java/com/vgs/pl/dao/impl/UserDaoImpl.java
@@ -0,0 +1,14 @@
+package com.vgs.pl.dao.impl;
+
+import com.vgs.pl.dao.UserDao;
+
+/**
+ * Created by Santosh.Kumar on 10/25/2016.
+ */
+public class UserDaoImpl implements UserDao {
+
+    @Override
+    public Object findUser(Object userInfo) throws Exception {
+        return null;
+    }
+}
